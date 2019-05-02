c86be3085961e3299c5cd914e75b078fe199e49a
YY <lyy0005@gmail.com>
2019-05-02 16:15:16 -0500

    Update cmd.sh

--- step_4/cmd.sh
+++ step_4/cmd.sh
@@ -11,3 +11,5 @@ tblastn -db assembly.fasta -query SP.fasta -out SP.fasta.blast -evalue 1e-10 -ou
 # api gene
 perl split_scaffolds_by_Ns.v1.0.pl api.fasta api.Ns.fasta
 blastn -db assembly.fasta -query api.Ns.fasta -out api.Ns.fasta.blast.out -evalue 1e-10 -outfmt "6 qlen slen qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore"
+# filter results with at least 98% identity and 98% sequence coverage
+less -S api.Ns.fasta.blast.out | perl -e 'while(<>){@a=split; $por=$a[5]/$a[0]; if(($a[4]>=98)&&($por>=0.98)){print}}' > api.Ns.fasta.blast.out.filtered
